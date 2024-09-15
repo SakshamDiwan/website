728de8019302068e1d84161adcd51d1aa3962fe4
Saksham Diwan <sd966@cornell.edu>
2024-09-15 13:59:32 -0400

    Buttons fixed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -50,7 +50,9 @@ const About = () => {
             <div>
               <p className='grid-headtext'>I work remotely across most timezones</p>
               <p className='grid-subtext'>I am based in America, with remote work available.</p>
-              <Button name="Contact Me" isBeam containerClass='w-full mt-10'/>
+              <a href="#contact">
+                <Button name="Contact Me" isBeam containerClass='w-full mt-10'/>
+              </a>
             </div>
           </div>
         </div>
