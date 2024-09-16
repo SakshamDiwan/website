5a303925fd0064739bd4034474b14a9a85aa8bca
Saksham Diwan <sd966@cornell.edu>
2024-09-16 12:14:58 -0400

    Few fixes made

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
   return (
     <footer className='c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5'>
       <div className="flex gap-3">
-        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="social-icon">
+        <a href="https://github.com/SakshamDiwan" target="_blank" rel="noreferrer" className="social-icon">
           <img src="./assets/github.svg" alt="github" className="w-1/2 h-1/2" />
         </a>
-        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="social-icon">
+        <a href="https://x.com/saks6280" target="_blank" rel="noreferrer" className="social-icon">
           <img src="./assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
         </a>
-        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="social-icon" >
-          <img src="./assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2"/>
+        <a href="https://www.linkedin.com/in/saksham-diwan-01b336237/" target="_blank" rel="noreferrer" className="social-icon" >
+          <img src="./assets/linkedin.svg" alt="linkedin" className="w-1/2 h-1/2"/>
         </a>
       </div>
 
