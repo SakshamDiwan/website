492e033d2d0e8de0220a7cf70b90876aa933c7b8
Saksham Diwan <sd966@cornell.edu>
2024-09-16 14:31:44 -0400

    Project Section Edit left

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -50,7 +50,7 @@ const Projects = () => {
               })}
             </div>
             <a className='flex items-center gap-2 cursor-pointer text-white-600' href={currentProject.href} target="_blank" rel='noreferrer'>
-              <p>Check Live Site</p>
+              <p>Link</p>
               <img src="./assets/arrow-up.png" alt="arrow" className='w-3 h-3'/>
             </a>
           </div>
