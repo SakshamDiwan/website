599a23473f71e2211bdfa3e88631bcbf49fd333d
Saksham298-create <saksham@getkatalyst.app>
2025-09-03 23:22:23 -0400

    Updated Experiences

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,120 +1,118 @@
 export const navLinks = [
   {
     id: 1,
-    name: 'Home',
-    href: '#home',
+    name: "Home",
+    href: "#home",
   },
   {
     id: 2,
-    name: 'About',
-    href: '#about',
+    name: "About",
+    href: "#about",
   },
   {
     id: 3,
-    name: 'Work',
-    href: '#work',
+    name: "Work",
+    href: "#work",
   },
   {
     id: 4,
-    name: 'Contact',
-    href: '#contact',
+    name: "Contact",
+    href: "#contact",
   },
 ];
 
 export const myProjects = [
   {
-    title: 'WalksOfLife.ai - Idea validation platform',
+    title: "WalksOfLife.ai - Idea validation platform",
     desc: "WalksOfLife.ai is a full-stack idea validation and feedback platform that creates human personas sampled from census data based on characteristics like age, education and occupation, through prompt engineering. It provides analytics, alpha testing and focus group features in the platform to validate a user's idea and give feedback on their product",
-    subdesc:
-      'Built using Next.js, Tailwind CSS, TypeScript, and Python.',
-    href: 'https://devpost.com/software/walksoflife/',
-    texture: './textures/project/project1.png',
-    logo: './assets/project-logo1.png',
+    subdesc: "Built using Next.js, Tailwind CSS, TypeScript, and Python.",
+    href: "https://devpost.com/software/walksoflife/",
+    texture: "./textures/project/project1.png",
+    logo: "./assets/project-logo1.png",
     logoStyle: {
-      backgroundColor: '#2A1816',
-      border: '0.2px solid #36201D',
-      boxShadow: '0px 0px 60px 0px #AA3C304D',
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
     },
-    spotlight: './assets/spotlight1.png',
+    spotlight: "./assets/spotlight1.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: './assets/react.svg',
+        name: "React.js",
+        path: "./assets/react.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: './assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "./assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: './assets/typescript.png',
+        name: "TypeScript",
+        path: "./assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: './assets/framer.png',
+        name: "Framer Motion",
+        path: "./assets/framer.png",
       },
     ],
   },
   {
-    title: 'TriCaml - Image Manipulation Tool',
-    desc: 'TriCaml is a image manipulation tool that allows users to manipulate images in various ways, such as brightening/dimming, flipping, applying 3d effects and many more with ease. Users can upload images, select their desired operations and receive the transformed image.',
-    subdesc:
-      'Built using OCaml and the camlimages library.',
-    href: 'https://github.com/SakshamDiwan/ImageManipulationLibrary/tree/main/ImageManipulation/',
-    texture: './textures/project/project4.png',
-    logo: './assets/project-logo4.png',
+    title: "TriCaml - Image Manipulation Tool",
+    desc: "TriCaml is a image manipulation tool that allows users to manipulate images in various ways, such as brightening/dimming, flipping, applying 3d effects and many more with ease. Users can upload images, select their desired operations and receive the transformed image.",
+    subdesc: "Built using OCaml and the camlimages library.",
+    href: "https://github.com/SakshamDiwan/ImageManipulationLibrary/tree/main/ImageManipulation/",
+    texture: "./textures/project/project4.png",
+    logo: "./assets/project-logo4.png",
     logoStyle: {
-      backgroundColor: '#0E1F38',
-      border: '0.2px solid #0E2D58',
-      boxShadow: '0px 0px 60px 0px #2F67B64D',
+      backgroundColor: "#0E1F38",
+      border: "0.2px solid #0E2D58",
+      boxShadow: "0px 0px 60px 0px #2F67B64D",
     },
-    spotlight: './assets/spotlight4.png',
+    spotlight: "./assets/spotlight4.png",
     tags: [
       {
         id: 1,
-        name: 'OCaml',
-        path: './assets/ocaml.svg',
+        name: "OCaml",
+        path: "./assets/ocaml.svg",
       },
     ],
   },
   {
-    title: 'Interview Wizard - AI Interview Prep Tool',
-    desc: 'Interview Wizard is a web application that makes interview training for candidates applying for jobs & internships easier by tailoring interview practice to the roles they apply to and also grading and analysing their interview answers!',
+    title: "Interview Wizard - AI Interview Prep Tool",
+    desc: "Interview Wizard is a web application that makes interview training for candidates applying for jobs & internships easier by tailoring interview practice to the roles they apply to and also grading and analysing their interview answers!",
     subdesc:
-      'Built using HTML, CSS, DeepFace, MongoDB, Modal, OpenAI API, and Django',
-    href: 'https://devpost.com/software/interview-wizard/',
-    texture: './textures/project/project5.png',
-    logo: './assets/project-logo5.png',
+      "Built using HTML, CSS, DeepFace, MongoDB, Modal, OpenAI API, and Django",
+    href: "https://devpost.com/software/interview-wizard/",
+    texture: "./textures/project/project5.png",
+    logo: "./assets/project-logo5.png",
     logoStyle: {
-      backgroundColor: '#1C1A43',
-      border: '0.2px solid #252262',
-      boxShadow: '0px 0px 60px 0px #635BFF4D',
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
     },
-    spotlight: './assets/spotlight5.png',
+    spotlight: "./assets/spotlight5.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: './assets/react.svg',
+        name: "React.js",
+        path: "./assets/react.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: './assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "./assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: './assets/typescript.png',
+        name: "TypeScript",
+        path: "./assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: './assets/framer.png',
+        name: "Framer Motion",
+        path: "./assets/framer.png",
       },
     ],
   },
@@ -124,57 +122,86 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
   return {
     deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
-    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
-    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
-    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    cubePosition: isSmall
+      ? [4, -5, 0]
+      : isMobile
+      ? [5, -5, 0]
+      : isTablet
+      ? [5, -5, 0]
+      : [9, -5.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+      ? [5, 4, 0]
+      : isTablet
+      ? [5, 4, 0]
+      : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-5, 7, 0]
+      : isMobile
+      ? [-10, 10, 0]
+      : isTablet
+      ? [-12, 10, 0]
+      : [-24, 10, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+      ? [-9, -10, -10]
+      : isTablet
+      ? [-11, -7, -10]
+      : [-13, -13, -10],
   };
 };
 
 export const workExperiences = [
   {
     id: 1,
-    name: 'Meta',
-    pos: 'Software Engineer Intern',
-    duration: 'May 2025 - Aug 2025',
-    title: "Working on scaling the Ads Manager Platform.",
-    icon: './assets/meta.svg',
-    animation: 'salute',
+    name: "Katalyst AI",
+    pos: "Software Engineer Intern",
+    duration: "Aug 2025 - Dec 2025",
+    title:
+      "Building full-stack AI agents in TypeScript to automate CRM(Salesforce, Hubspot, Attio) and account planning workflows for B2B sales teams",
+    icon: "./assets/katalyst.svg",
+    animation: "clapping",
   },
   {
     id: 2,
-    name: 'Katalyst AI',
-    pos: 'Software Engineer Intern',
-    duration: 'Dec 2024 - Jan 2025',
-    title: "Built both the frontend and the backend of an AI agent that lets users query employee data from Apollo using natural language using TypeScript, React, Tailwind, and the Vercel AI SDK.",
-    icon: './assets/katalyst.svg',
-    animation: 'clapping',
+    name: "Meta",
+    pos: "Software Engineer Intern",
+    duration: "May 2025 - Aug 2025",
+    title: "Working on scaling the Ads Manager Platform.",
+    icon: "./assets/meta.svg",
+    animation: "salute",
   },
+
   {
     id: 3,
-    name: '121 Health',
-    pos: 'Software Engineer Intern',
-    duration: 'Jun 2024 - Aug 2024',
-    title: "Engineered a TypeScript chatbot with GPT API, cutting response times by 40%. Developed a Bayesian algorithm with Firebase’s BigQuery, increasing user interaction by 15%. Optimized a machine learning pipeline and SQL workflows, growing an app to 600 users.",
-    icon: './assets/121.svg',
-    animation: 'victory',
+    name: "121 Health",
+    pos: "Software Engineer Intern",
+    duration: "Jun 2024 - Aug 2024",
+    title:
+      "Engineered a TypeScript chatbot with GPT API, cutting response times by 40%. Developed a Bayesian algorithm with Firebase’s BigQuery, increasing user interaction by 15%. Optimized a machine learning pipeline and SQL workflows, growing an app to 600 users.",
+    icon: "./assets/121.svg",
+    animation: "victory",
   },
   {
     id: 4,
-    name: 'Signal Science Lab',
-    pos: 'Undergraduate Research Intern',
-    duration: 'Mar 2024 - Present',
-    title: "Analyzed a 28M-row electricity substation dataset, achieving a 10x reduction in processing times while clustering users based on usage patterns. Used Dynamic Time Warping and k-means clustering to group 7000 users for infrastructure insights. Visualized findings to inform load balancing strategies, contributing to a research paper in progress.",
-    icon: './assets/cornell.svg',
-    animation: 'clapping',
+    name: "Signal Science Lab",
+    pos: "Undergraduate Research Intern",
+    duration: "Mar 2024 - Present",
+    title:
+      "Analyzed a 28M-row electricity substation dataset, achieving a 10x reduction in processing times while clustering users based on usage patterns. Used Dynamic Time Warping and k-means clustering to group 7000 users for infrastructure insights. Visualized findings to inform load balancing strategies, contributing to a research paper in progress.",
+    icon: "./assets/cornell.svg",
+    animation: "clapping",
   },
   {
     id: 5,
-    name: 'Filmify (Student Startup)',
-    pos: 'Software Engineer',
-    duration: 'Mar 2024 - Present',
-    title: "Built a full-stack production compliance platform with Next.js, TypeScript, Tailwind, and Supabase. Added RAG-based chat with callsheets, AI-powered PDF parsing, role-based access, and real-time email alerts.",
-    icon: './assets/filmify.svg',
-    animation: 'salute',
+    name: "Filmify (Student Startup)",
+    pos: "Software Engineer",
+    duration: "Mar 2024 - May 2025",
+    title:
+      "Built a full-stack production compliance platform with Next.js, TypeScript, Tailwind, and Supabase. Added RAG-based chat with callsheets, AI-powered PDF parsing, role-based access, and real-time email alerts.",
+    icon: "./assets/filmify.svg",
+    animation: "salute",
   },
 ];
